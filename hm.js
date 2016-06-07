24500e386da0f435b541dc628ad3424846f52a38
axjs <a-x@ukr.net>
2016-06-07 23:48:45 +0300

    Update hm.js

--- hm.js
+++ hm.js
@@ -1,3 +1,5 @@
+console.info('Start')
+
 pjs.addSuite({
   // single URL or array
   url: 'http://www2.hm.com/en_gb/sale/kids/viewall.html?product-type=kids_all&sort=stock&offset=0&page-size=5',
@@ -12,7 +14,10 @@ pjs.addSuite({
         image: $(el).find('img').attr('data-image')
       };
       
+      console.info(res)
       return res;
     });
   }
 });
+
+console.info('Stop')
